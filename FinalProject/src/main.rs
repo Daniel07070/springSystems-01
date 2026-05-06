8628893f6520ffca3ada11b94a90b13e04e06d54
Daniel07070 <130100177+Daniel07070@users.noreply.github.com>
2026-05-06 02:58:59 +0000

    Final project code completed

--- FinalProject/src/main.rs
+++ FinalProject/src/main.rs
@@ -18,6 +18,10 @@ struct Task {
     duration: u64,
     kind: TaskType,
     cpu_cost: u64,
+
+    start_time: Option<u64>,
+    finish_time: Option<u64>,
+
 }
 
 // -------------------- TASK GENERATOR --------------------
@@ -38,6 +42,8 @@ fn generate_tasks(n: usize) -> Vec<Task> {
             duration: 200,
             kind,
             cpu_cost,
+            start_time: None,
+            finish_time: None,
         });
     }
 
@@ -51,6 +57,10 @@ struct Stats {
     total_time: u64,
     busy_time: u64,
     completed_tasks: u64,
+    total_wait_time: u64,
+    total_turnaround_time: u64,
+    io_completed: u64,
+    cpu_completed: u64,
 }
 
 // -------------------- MAIN --------------------
@@ -70,17 +80,41 @@ fn main() {
     // =====================================================
     // MONITOR THREAD
     // =====================================================
-    {
-        let stats = Arc::clone(&stats);
+    
+        let stats_monitor = Arc::clone(&stats);
 
-        thread::spawn(move || {
+        let monitor_handle = thread::spawn(move || {
             loop {
                 thread::sleep(Duration::from_secs(1));
 
-                let s = stats.lock().unwrap();
+                let s = stats_monitor.lock().unwrap();
+
+                // if s.completed_tasks == 1000{
+                //     println!("\nFinal stats reached. Monitor exiting...");
+                //     println!("\n===== MONITOR =====");
+                //     println!("Completed tasks: {}", s.completed_tasks);
+                //     println!("Busy time: {}", s.busy_time);
+                //     println!("Total time: {}", s.total_time);
+                //     println!("CPU usage: {:.2}%", utilization);
+                //     println!("Avg wait time: {:.2}", avg_wait);
+                //     println!("Avg turnaround time: {:.2}", avg_turnaround);
+                //     println!("===================\n");
+                //     break;
+                // }
 
                 let utilization = if s.total_time > 0 {
                     (s.busy_time as f64 / s.total_time as f64) * 100.0
+                } else {
+                    0.0
+                };
+                let avg_wait = if s.completed_tasks > 0 {
+                    s.total_wait_time as f64 / s.completed_tasks as f64
+                } else {
+                     0.0
+                };
+
+                let avg_turnaround = if s.completed_tasks > 0 {
+                  s.total_turnaround_time as f64 / s.completed_tasks as f64
                 } else {
                     0.0
                 };
@@ -90,20 +124,41 @@ fn main() {
                 println!("Busy time: {}", s.busy_time);
                 println!("Total time: {}", s.total_time);
                 println!("CPU usage: {:.2}%", utilization);
+                println!("IO tasks completed: {}", s.io_completed);
+                println!("CPU tasks completed: {}", s.cpu_completed);
+                println!("Avg wait time: {:.2}", avg_wait);
+                println!("Avg turnaround time: {:.2}", avg_turnaround);
                 println!("===================\n");
+
+                if s.completed_tasks == 1000{
+                    println!("\nFinal stats reached. Monitor exiting...");
+                    println!("\n===== MONITOR =====");
+                    println!("Completed tasks: {}", s.completed_tasks);
+                    println!("Busy time: {}", s.busy_time);
+                    println!("Total time: {}", s.total_time);
+                    println!("CPU usage: {:.2}%", utilization);
+                    println!("IO tasks completed: {}", s.io_completed);
+                    println!("CPU tasks completed: {}", s.cpu_completed);
+                    println!("Avg wait time: {:.2}", avg_wait);
+                    println!("Avg turnaround time: {:.2}", avg_turnaround);
+                    println!("===================\n");
+                    break;
+                }
             }
         });
-    }
+    
 
     // =====================================================
     // WORKER POOL (8 WORKERS)
     // =====================================================
+    let mut worker_handles = vec![];
+
     for worker_id in 0..8 {
         let rx_work = Arc::clone(&rx_work);
         let tx_done = tx_done.clone();
         let stats = Arc::clone(&stats);
 
-        thread::spawn(move || {
+        let handle = thread::spawn(move || {
             loop {
                 let task = {
                     let lock = rx_work.lock().unwrap();
@@ -120,7 +175,7 @@ fn main() {
                         let run_time = task.cpu_cost;
                         task.cpu_cost = 0;
 
-                        thread::sleep(Duration::from_millis(1));
+                        thread::sleep(Duration::from_millis(task.cpu_cost as u64));
 
                         {
                             let mut s = stats.lock().unwrap();
@@ -130,6 +185,12 @@ fn main() {
                         {
                             let mut s = stats.lock().unwrap();
                             s.completed_tasks += 1;
+
+                            match task.kind{
+                                TaskType::IO => s.io_completed += 1,
+                                TaskType::CPU => s.cpu_completed += 1,
+
+                            }
                         }
 
                         println!(
@@ -138,21 +199,28 @@ fn main() {
                         );
 
                         // ONLY report completion (no requeue)
-                        tx_done.send(task).unwrap();
+                        //tx_done.send(task).unwrap();
+                        task.finish_time = Some({
+                            let s = stats.lock().unwrap();
+                            s.total_time
+                        });
+
+                        let _ = tx_done.send(task);//.unwrap();
                     }
                     Err(_) => break,
                 }
             }
         });
+        worker_handles.push(handle);
     }
 
     // =====================================================
     // MANAGER (SOLE SCHEDULER)
     // =====================================================
-    let tx_work = tx_work.clone();
+    let tx_work_manager = tx_work.clone();
     let stats_manager = Arc::clone(&stats);
 
-    thread::spawn(move || {
+    let manager_handle = thread::spawn(move || {
         let mut ready_queue: VecDeque<Task> = VecDeque::new();
         let mut time: u64 = 0;
         let mut i = 0;
@@ -179,8 +247,18 @@ fn main() {
             }
 
             // receive completed tasks (no requeue anymore, just accounting)
-            while let Ok(_task) = rx_done.try_recv() {
-                // no requeue needed in strict model
+            // while let Ok(_task) = rx_done.try_recv() {
+            //     // no requeue needed in strict model
+            // }
+            while let Ok(task) = rx_done.try_recv() {
+                if let (Some(start), Some(finish)) = (task.start_time, task.finish_time) {
+                    let wait = start - task.arrival_time;
+                    let turnaround = finish - task.arrival_time;
+
+                    let mut s = stats_manager.lock().unwrap();
+                     s.total_wait_time += wait;
+                    s.total_turnaround_time += turnaround;
+                }
             }
 
             // schedule next task
@@ -192,7 +270,13 @@ fn main() {
 
                 if cpu_used + cost <= 100.0 {
                     cpu_used += cost;
-                    tx_work.send(task).unwrap();
+                    //
+                    let mut task = task;
+
+                    if task.start_time.is_none() {
+                        task.start_time = Some(time);
+                    }
+                    tx_work_manager.send(task).unwrap();
                 } else {
                     ready_queue.push_back(task);
                 }
@@ -206,8 +290,23 @@ fn main() {
         }
     });
 
-    // keep program alive
-    loop {
-        thread::park();
+
+    // close program
+    // wait for manager to finish
+    manager_handle.join().unwrap();
+
+    // close channel → workers exit
+    drop(tx_work);
+
+    // wait for all workers
+    for handle in worker_handles {
+        handle.join().unwrap();
     }
+
+    // wait for monitor
+    monitor_handle.join().unwrap();
+
+    println!("Program finished cleanly.");
+    
+    
 }
